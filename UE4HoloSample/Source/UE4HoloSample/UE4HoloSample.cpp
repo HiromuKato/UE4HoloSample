76c0119610bdd315f37e3e514b217638b71c3b1b
HiromuKato <hiromu1212@gmail.com>
2020-12-09 22:17:22 +0900

    git rm -r --cached . でキャッシュ削除

--- UE4HoloSample/Source/UE4HoloSample/UE4HoloSample.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE4HoloSample.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4HoloSample, "UE4HoloSample" );
--- UE4HoloSample/Source/UE4HoloSample/UE4HoloSampleGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "UE4HoloSampleGameModeBase.h"
-
