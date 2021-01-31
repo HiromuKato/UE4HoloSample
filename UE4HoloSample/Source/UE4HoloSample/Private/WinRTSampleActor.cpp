8bb678274e56d2eea5c9888599f61ccd5634a118
HiromuKato <hiromu1212@gmail.com>
2021-01-31 12:46:35 +0900

    ボタンタップでC++のコードを実行する部分までの実装

---
+++ UE4HoloSample/Source/UE4HoloSample/Private/WinRTSampleActor.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WinRTSampleActor.h"
+
+// Sets default values
+AWinRTSampleActor::AWinRTSampleActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWinRTSampleActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// キーが -1 の場合、上書きされずに常に新メッセージとして表示される
+	const int32 AlwaysAddKey = -1;
+	GEngine->AddOnScreenDebugMessage(AlwaysAddKey, 5.0f, FColor::Yellow, "AWinRTSampleActor::BeginPlay");
+
+}
+
+// Called every frame
+void AWinRTSampleActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AWinRTSampleActor::SampleFunc() {
+	UE_LOG(LogTemp, Display, TEXT("ログ出力サンプル"));
+
+	const int32 AlwaysAddKey = -1;
+	GEngine->AddOnScreenDebugMessage(AlwaysAddKey, 5.0f, FColor::Yellow, "AWinRTSampleActor::SampleFunc");
+}
+
---
+++ UE4HoloSample/Source/UE4HoloSample/Public/WinRTSampleActor.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WinRTSampleActor.generated.h"
+
+UCLASS()
+class UE4HOLOSAMPLE_API AWinRTSampleActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWinRTSampleActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "WinRTSample")
+		void SampleFunc();
+
+};
